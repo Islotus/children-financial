98577d8c2e0441852c673ca245311859d83d46d3
Islotus <2791201278@qq.com>
2019-07-24 16:01:52 +0800

    初始框架

---
+++ src/main/java/com/czbank/childrenfinancial/ChildrenFinancialApplication.java
@@ -0,0 +1,13 @@
+package com.czbank.childrenfinancial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildrenFinancialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChildrenFinancialApplication.class, args);
+    }
+
+}
