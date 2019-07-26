3effca3be6ab514413d34e278a3e848a0f6562df
niceLafeier <2310598704@qq.com>
2019-07-26 15:59:53 +0800

    上传UserInfMapper文件，增加了用户基本信息维护的功能接口

--- src/main/java/com/czbank/childrenfinancial/mapper/UserInfMapper.java
+++ src/main/java/com/czbank/childrenfinancial/mapper/UserInfMapper.java
@@ -4,12 +4,13 @@ import com.czbank.childrenfinancial.po.UserInf;
 
 import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Repository;
+import tk.mybatis.mapper.common.Mapper;
 
 import java.util.List;
 
-@Mapper
+@org.apache.ibatis.annotations.Mapper
 @Repository
-public interface UserInfMapper {
+public interface UserInfMapper extends Mapper<UserInf> {
     //0insert
     @Insert("insert into user_inf(userId,account,relatedAccount,name,isParent,idCard,birthday,phoneNbr,loginPw,openTime)" +
             "values(#{userId},#{account},#{relatedAccount},#{name},#{isParent},#{idCard},#{birthday},#{phoneNbr},#{loginPw},#{openTime})")
@@ -31,7 +32,15 @@ public interface UserInfMapper {
     @Select("select * from user_inf where ACCOUNT = #{account} and LOGIN_PW = #{loginPw} limit 1")
     public UserInf selectByAccountPw(UserInf userInf);
 
+    //用户管理模块
+    @Select("select * from user_inf where account=#{account} limit 1")
+    UserInf selectOne(UserInf userInf);
 
+    @Select("update user_inf set PHONE_NBR=#{phoneNbr} where ACCOUNT=#{account}")
+    void updatePhoneNbr(String account, String phoneNbr);
+
+    @Select("update user_inf set LOGIN_PW=#{loginPw} where ACCOUNT=#{account}")
+    void updateLoginPw(String account, String loginPw);
 
 
 
